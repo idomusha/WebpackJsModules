d8db994b7079ddad802a1ed8ad143c86e885917f
idomusha <idomusha@gmail.com>
2017-05-02 12:01:55 +0200

    refactor CommonJS into ES6 modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const sum = require('./sum');
+// const sum = require('./sum');
+import sum from './sum';
 
 let total = sum(10, 5);
 console.log(total);
--- src/sum.js
+++ src/sum.js
@@ -1,3 +1,4 @@
 const sum = (a, b) => a + b;
 
-module.exports = sum;
+// module.exports = sum;
+export default sum;
